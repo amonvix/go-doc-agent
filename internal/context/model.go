c95fbc20288b7a2cc6faf68be3fbd4592c211b7a
amonvix <hspedroso@gmail.com>
2026-01-19 21:21:35 -0300

    structure: all necessary adjusts made in model and templates.

--- internal/context/model.go
+++ internal/context/model.go
@@ -1,5 +1,9 @@
 package context
 
+// ---------------------------
+// Type system
+// ---------------------------
+
 type TypeKind string
 
 const (
@@ -16,4 +20,35 @@ const (
 type TypeInfo struct {
 	Name string
 	Kind TypeKind
+}
+
+// ---------------------------
+// Function model
+// ---------------------------
+
+type Function struct {
+	Name        string
+	Description string
+
+	Params  []Param
+	Returns []Return
+}
+
+// ---------------------------
+// Parameters
+// ---------------------------
+
+type Param struct {
+	Name        string
+	Type        TypeInfo
+	Description string
+}
+
+// ---------------------------
+// Return values
+// ---------------------------
+
+type Return struct {
+	Type        TypeInfo
+	Description string
 }
